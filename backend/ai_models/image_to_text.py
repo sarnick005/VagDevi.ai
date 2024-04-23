67143c2df7dcbf66402371c4a1f49eda2e9564b4
sarnick005 <sarnick.chakraborty@gmail.com>
2024-04-23 14:41:02 +0530

    updated

--- backend/ai_models/image_to_text.py
+++ backend/ai_models/image_to_text.py
@@ -1,12 +1,13 @@
 from PIL import Image
 import os
 import google.generativeai as genai
-
+from config import Config
+GEMINI_API_KEY = Config.GEMINI_API_KEY
 
 def imageToText(prompt, image_path):
     try:
         image = Image.open(image_path)
-        genai.configure(api_key="AIzaSyCoAT805oVBf-ToA_9H3MjI99nD7HuXoBk")
+        genai.configure(api_key=GEMINI_API_KEY)
         model = genai.GenerativeModel("gemini-pro-vision")
         prompt = [prompt, image]
         response = model.generate_content(prompt, stream=True)
